430013f21794bc5a9942467cac18af3099567d8b
Muzeric <muzer@mail.ru>
2017-01-24 06:29:04 +0300

    Room autoconfig, part 4: needList Fix style guide

--- default/main.js
+++ default/main.js
@@ -45,17 +45,18 @@ var statClass = require('stat');
 var stat = statClass.init();
 
 module.exports.loop = function () {
-    var error_count = {};
-    var allRoles = {};
+    var moveErrors = {};
+    var rolesCount = {};
+    var objectCache = {};
     
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
-            console.log(name + " DEAD (" + Memory.creeps[name].spawnName + ")");
+            console.log(name + " DEAD (" + Memory.creeps[name].roomName + ")");
             statClass.die(name);
             delete Memory.creeps[name];
         } else if (Game.creeps[name].memory.errors > 0) {
             console.log(name + " has "+ Game.creeps[name].memory.errors + " errors");
-            error_count[Game.creeps[name].memory.spawnName] = (error_count[Game.creeps[name].memory.spawnName] || 0) + 1;
+            moveErrors[Game.creeps[name].memory.roomName] = 1;
         }
     }
 
@@ -64,26 +65,24 @@ module.exports.loop = function () {
         if(creep.spawning) {
             continue;
         }
-        if(!allRoles[creep.memory.role]) {
-            allRoles[creep.memory.role] = {
-                "count" : {},
-                "obj" : require('role.' + creep.memory.role),
-            };
-        }
+        let role = creep.memory.role;
+        if(!role in rolesCount)
+            rolesCount[role] = {};
+        if(!role in objectCache)
+            objectCache[role] = require('role.' + role);
         
         if (creep.ticksToLive > 200)
-            allRoles[creep.memory.role]["count"][creep.memory.spawnName] = (allRoles[creep.memory.role]["count"][creep.memory.spawnName] || 0) + 1;
+            rolesCount[role][creep.memory.spawnName] = (rolesCount[role][creep.memory.spawnName] || 0) + 1;
         
-        if(error_count[creep.memory.spawnName]) {
-            if(creep.moveTo(creep.room.controller) == OK) {
+        if(moveErrors[creep.memory.roomName]) {
+            if(creep.moveTo(creep.room.controller) == OK)
                 creep.memory.errors = 0;
-            }
             continue;
         }
         
         let lastCPU = Game.cpu.getUsed();
         
-        allRoles[creep.memory.role].obj.run(creep);
+        objectCache[role].run(creep);
             
         creep.memory.stat.CPU += (Game.cpu.getUsed() - lastCPU);
 
@@ -101,14 +100,15 @@ module.exports.loop = function () {
         
     }
     
-    stat.roles = JSON.parse(JSON.stringify(allRoles));
+    stat.roles = JSON.parse(JSON.stringify(rolesCount));
     
 if(0) {    
-    
     if (!Memory.limitList || !Memory.limitTime) {
         Memory.limitList = {};
         Memory.limitTime = {};
     }
+    let needList = [];
+    let lastCPU = Game.cpu.getUsed();
     _.forEach(_.filter(Game.rooms, r => r.controller.my), function(room) {
         let creepsCount =  _.countBy(_.filter(Game.creeps, c => c.memory.roomName == room.name && c.ticksToLive > 200), 'memory.role'); 
 
@@ -117,35 +117,40 @@ if(0) {
             Memory.limitTime[room.name] = Game.time;
         }
 
-        let spawns = room.find(FIND_MY_SPAWNS, {filter : s => !s.spawning});
-        if (!spawns.length) {
-            console.log(room.name + ": all spawns are spawning");
-            //return true;
+        for (let limit of Memory.limitList[room.name]) {
+            if ((creepsCount[limit.role] || 0) < limit.count)
+                needList.push(limit);
         }
 
-        let minEnergy = 300;
-        let canRepair = 0;
-        for (let limit of Memory.limitList[room.name]) {
-            if(!spawns.length)
-                break;
-            if (limit.count <= 0)
-                continue;
+        let canRepair = creepsCount["upgrader"] ? 1 : 0;
+        // TODO tower actions
+    }); // each room end
+    
+    console.log("needList: CPU=" + _.floor(Game.cpu.getUsed() - lastCPU, 2) + "; list=" + JSON.stringify(needList));
+    lastCPU = Game.cpu.getUsed();
 
-            if (limit.role == "ENERGY") {
-                minEnergy = limit.count;
-                continue;
-            } else if (limit.role == "REPAIR") {
-                canRepair = limit.count;
-                continue;
-            }
-            if(!allRoles[limit.role]) {
-                allRoles[limit.role] = {
-                    "count" : {},
-                    "obj" : require('role.' + limit.role),
-                };
-            }
-
-            if ((creepsCount[limit.role] || 0) < limit.count) {
+    let skipSpawnNames = {};
+    for (let need of needList.sort(function(a,b) { return (a.priority - b.priority) || (a.wishEnergy - b.wishEnergy); } )) {
+        if (!_.filter(Game.spawns, s => !s.spawning && !s.name in skipSpawnNames).length) {
+            console.log("All spawns are spawning");
+            break;
+        }
+        
+        let res = getSpawnForCreate(need, skipSpawnNames);
+        if (res[0] == -2) {
+            console.log("needList: " + need.role + " has no spawns in range");
+        } else if (res[0] == -1) {
+            if (res[1])
+                skipSpawnNames[res[1]] = 1;
+            console.log("needList: " + need.role + " return waitSpawnName=" + res[1]);
+        } else if (res[0] == -3) {
+            console.log("needList: " + need.role + " has no spawns with enough energyCapacity");
+        } else if (res[0] == 0) {
+            let spawn = res[1];
+            console.log("needList: " + need.role + " for room " + need.roomName + " creation by " + spawn.name + " with energy " + spawn.room.energyAvailable);
+        }
+    }
+/*
                 // Need, but not enough energy, so break & WAIT
                 if (limit.emin && room.energyAvailable < room.energyCapacityAvailable && room.energyAvailable < limit.emin)
                     break;
@@ -158,8 +163,11 @@ if(0) {
                     let energy = room.energyAvailable;
                     let spawn = spawns.pop();
                     console.log(room.name + ": tries to create " + limit.role + " with energy=" + energy);
-                    /*
-                    let res = allRoles[limit.role].obj.create(spawn.name, role, energy);
+
+                    if(!role in objectCache)
+                        objectCache[role] = require('role.' + role);
+
+                    let res = objectCache[role].create(spawn.name, role, energy);
                     
                     if(Game.creeps[res[0]]) {
                         let creepm = Game.creeps[res[0]].memory;
@@ -175,13 +183,8 @@ if(0) {
                     }
 
                     console.log(res[0] + " BORN by " + spawnName + ", energy (" + energy + "->" + res[2] + ":" + (energy - res[2]) + ") [" + res[1] + "]");
-                    */
                 }
-            }
-
-        } // limitList end
-        
-    });
+*/
 
 }
     
@@ -204,7 +207,7 @@ if(0) {
             }
             let addCheck = {
                 longbuilder : utils.getLongBuilderTargets() ? 1 : 0,
-                builder : ((allRoles["builder"] ? allRoles["builder"].count[spawnName] : 0) < cs.length + rs.length),
+                builder : ((rolesCount["builder"] ? rolesCount["builder"][spawnName] : 0) < cs.length + rs.length),
             };
 
             let addCount = {
@@ -235,7 +238,7 @@ if(0) {
                 let role = arr[0];
                 let climit = arr[1];
                 let emin = arr[2];
-                let count = (allRoles[role] ? (allRoles[role].count[spawnName]||0) : 0);
+                let count = (rolesCount[role] ? (rolesCount[role][spawnName]||0) : 0);
     
                 //if (spawnName == "Spawn2")
                 //    console.log(spawnName + " check " + role + "; climit=" + climit + "; count=" + count + "; addCount=" + addCount[role]);
@@ -252,12 +255,9 @@ if(0) {
                 }
                 if (role in addCheck && !addCheck[role])
                     continue;
-                if(!allRoles[role]) {
-                    allRoles[role] = {
-                        "count" : {},
-                        "obj" : require('role.' + role),
-                    };
-                }
+                if(!role in objectCache)
+                    objectCache[role] = require('role.' + role);
+
                 if (
                     role in addCount && addCount[role] > 0 ||
                     !(role in addCount) && count < climit
@@ -272,7 +272,7 @@ if(0) {
                         spawn.room.energyAvailable >= minEnergy
                     ) {
                         let energy = spawn.room.energyAvailable;
-                        let res = allRoles[role].obj.create(spawnName, role, energy);
+                        let res = objectCache[role].create(spawnName, role, energy);
                         
                         if(Game.creeps[res[0]]) {
                             let creepm = Game.creeps[res[0]].memory;
@@ -347,46 +347,83 @@ function getLimits (room, creepsCount) {
     let repairLimit = utils.roomConfig[room.name].repairLimit || 100000;
     scount["repair"] = room.find(FIND_STRUCTURES, { filter : s => s.hits < s.hitsMax*0.9 && s.hits < repairLimit }).length;
     
-    let limit = [];
-    limit.push({
+    let limits = [];
+    limits.push({
             "role" : "harvester",
             "count" : 1,
             "args" : [scount[STRUCTURE_CONTAINER] && creepsCount["miner"] ? 0 : 1],
             "priority" : 1,
-    },{
-            "role" : "ENERGY",
-            "count" : 1500,
+            "wishEnergy" : 300,
     },{
             "role" : "miner",
             "count" : _.min([scount[STRUCTURE_CONTAINER], scount["source"]], 1),
             "priority" : 1,
+            "wishEnergy" : 650,
     },{
             "role" : "harvester",
             "count" : _.ceil((scount[STRUCTURE_EXTENSION] || 0) / 15) + _.floor((scount[STRUCTURE_TOWER] || 0) / 3),
             "args" : [scount[STRUCTURE_CONTAINER] && creepsCount["miner"] ? 0 : 1],
             "priority" : 2,
+            "wishEnergy" : 1350,
     },{
             "role" : "miner",
             "count" : _.min([scount[STRUCTURE_CONTAINER], scount["source"]]),
             "priority" : 2,
+            "wishEnergy" : 650,
     },{
             role : "upgrader",
             "count" : scount["source"],
             "priority" : 3,
-    },{
-            "role" : "REPAIR",
-            "count" : 1,
+            "wishEnergy" : 1500,
     },{
             "role" : "builder",
             "count" : (scount["construction"] || scount["repair"] && !scount[STRUCTURE_TOWER]) ? 1 : 0,
             "priority" : 4,
+            "wishEnergy" : 1500,
     },{
             "role" : "shortminer",
             "count" : (scount[STRUCTURE_LINK] >= 2 && scount[STRUCTURE_STORAGE] && creepsCount["longharvester"]) ? 1 : 0,
             "priority" : 5,
+            "wishEnergy" : 300,
     });
 
-    console.log(room.name + ": CPU=" + _.floor(Game.cpu.getUsed() - lastCPU, 2) + "; count=" + JSON.stringify(limit));
+    for (let limit of limits) {
+        limit["roomName"] = room.name;
+        limit["originalEnergyCapacity"] = room.energyCapacityAvailable;
+        limit["range"] = 2;
+        if (!"minEnergy" in limit)
+            limit["minEnergy"] = 0;
+    }
 
-    return limit;
+    console.log(room.name + ": CPU=" + _.floor(Game.cpu.getUsed() - lastCPU, 2) + "; limits=" + JSON.stringify(limits));
+
+    return limits;
+}
+
+function getSpawnForCreate (need, skipSpawnNames) {
+    let spawnsInRange = _.filter(Game.spawns, s => Game.map.getRoomLinearDistance(s.room.name, need.roomName) <= need.range && !s.spawning && !s.name in skipSpawnNames);
+    
+    if (!spawnsInRange.length)
+        return [-2];
+    
+    //if (need.minEnergy && _.maxBy(spawnsInRange, function(s) {return s.room.energyCapacityAvailable} ).room.energyCapacityAvailable < need.minEnergy)
+    //    return [-3];
+
+    let waitSpawnName = null;
+    for (let spawn of spawnsInRange.sort( function(a,b) { 
+        return (Game.map.getRoomLinearDistance(a.room.name, need.roomName) - Game.map.getRoomLinearDistance(b.room.name, need.roomName)) || (b.room.energyAvailable - a.room.energyAvailable); 
+    } )) {
+        if (
+            spawn.room.energyAvailable >= need.minEnergy &&
+            (
+                spawn.room.energyAvailable >= need.wishEnergy ||
+                spawn.room.energyAvailable >= spawn.room.energyCapacityAvailable && spawn.room.energyAvailable >= need.originalEnergyCapacity
+            )
+        )
+            return [0, spawn];
+        else if (!waitSpawnName && spawn.room.energyCapacityAvailable >= need.minEnergy)
+            waitSpawnName = spawn.name;
+    }
+
+    return [-1, waitSpawnName];
 }
