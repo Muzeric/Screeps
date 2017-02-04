19f1876065118a95760e5fa7bed986a2c19a9c43
Muzeric <muzer@mail.ru>
2017-02-05 02:39:58 +0300

    Repair containers by longminers

--- default/role.longminer.js
+++ default/role.longminer.js
@@ -68,6 +68,8 @@ var role = {
             if(creep.pos.isNearTo(source) && creep.pos.isNearTo(container)) {
                 if(creep.carry.energy < creep.carryCapacity)
                     creep.harvest(source);
+                if (container.hits < container.hitsMax * 0.95)
+                    creep.repair(container);
                 creep.transfer(container, RESOURCE_ENERGY);
             } else {
                 if (creep.pos.isNearTo(container))
@@ -86,6 +88,8 @@ var role = {
                 if(creep.harvest(source) == ERR_NOT_IN_RANGE)
                     creep.moveTo(source);
             else
+                if (container.hits < container.hitsMax * 0.95)
+                    creep.repair(container);
                 if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
                     creep.moveTo(container);
         }
