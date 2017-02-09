c3c11eb6cfbd2a1ec372f6a2394fd12729a5620e
Muzeric <muzer@mail.ru>
2017-02-10 02:21:12 +0300

    Don't hang on room border while warning

--- default/role.longharvester.js
+++ default/role.longharvester.js
@@ -10,6 +10,8 @@ var role = {
 
 		if (Memory.warning[creep.memory.roomName] > 1) {
 			creep.say("AAA");
+			if (creep.pos.x == 49 || creep.pos.y == 49 || creep.pos.x == 0 || creep.pos.y == 0)
+				creep.moveTo(Game.spawns[creep.memory.spawnName].room.controller);
 			return;
 		}
 
