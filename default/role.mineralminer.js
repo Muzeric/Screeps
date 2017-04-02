73b00c7d4069a66b6472ffc2f4d8662c05fa3d12
Muzeric <muzer@mail.ru>
2017-04-02 05:52:36 +0300

    Mineralminers goto to main room controller

--- default/role.mineralminer.js
+++ default/role.mineralminer.js
@@ -5,14 +5,14 @@ var role = {
     run: function(creep) {
         if (Game.roomsHelper.getHostilesCount(creep.room.name) > 0) {
 			creep.say("AAA");
-			creep.moveTo(Game.spawns[creep.memory.spawnName].room.controller);
+			creep.moveTo(Game.rooms[creep.memory.roomName].controller);
 			return;
 		}
 
         if (Game.roomsHelper.getHostilesCount(creep.memory.roomName) > 0) {
             creep.say("AAA");
             if (creep.pos.isBorder())
-                creep.moveTo(Game.spawns[creep.memory.spawnName].room.controller);
+                creep.moveTo(Game.rooms[creep.memory.roomName].controller);
             return;
         }
 
