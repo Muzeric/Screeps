a30047003762dbd59801324d43df3f7697ec02a1
Muzeric <muzer@mail.ru>
2017-04-02 06:14:11 +0300

    Revert AAA logic

--- default/role.longminer.js
+++ default/role.longminer.js
@@ -3,13 +3,10 @@ const profiler = require('screeps-profiler');
 
 var role = {
     run: function(creep) {
-        if (Game.roomsHelper.getHostilesCount(creep.room.name) > 1 || (creep.memory.lastHostile && creep.pos.isBorder())) {
+        if (Game.roomsHelper.getHostilesCount(creep.room.name) > 1) {
 			creep.say("AAA");
 			creep.moveTo(Game.spawns[creep.memory.spawnName].room.controller);
-            creep.memory.lastHostile = 1;
 			return;
-        } else {
-            creep.memory.lastHostile = 0;
         }
 
 		if (Game.roomsHelper.getHostilesCount(creep.memory.roomName) > 1) {
