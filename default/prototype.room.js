bf771d201803024b3a4a28c80b231a319be0979c
Muzeric <muzer@mail.ru>
2017-08-19 04:49:45 +0300

    Autobalancing minerals

--- default/prototype.room.js
+++ default/prototype.room.js
@@ -138,15 +138,15 @@ Room.prototype.balanceStore = function () {
             let est = global.cache.queueTransport.getStoreWithReserved(this.storage, rt) - BALANCE_MAX;
             //console.log(`${this.name}: has ${store[rt]} of ${rt} and est = ${est}`);
             for (let roomName in Game.rooms) {
+                if (est <= 0)
+                    break;
                 let room = Game.rooms[roomName];
                 if (roomName == this.name || !room.controller || !room.controller.my || !room.storage || global.cache.queueTransport.getStoreWithReserved(room.storage, rt) >= BALANCE_MIN)
                     continue;
                 let amount = _.min([est, BALANCE_MIN - global.cache.queueTransport.getStoreWithReserved(room.storage, rt)]);
                 console.log(`${this.name}: balance ${amount} of ${rt} to ${room.name}`);
-                //global.cache.queueTransport.addRequest(this.storage, room.storage, rt, amount);
+                global.cache.queueTransport.addRequest(this.storage, room.storage, rt, amount);
                 est -= amount;
-                if (est <= 0)
-                    break;
             }
         }
     }
