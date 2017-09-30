66e440b6a26f35fd0b5d606f93e3fb713aabbc2e
Muzeric <muzer@mail.ru>
2017-09-30 21:13:35 +0300

    Fix ref

--- default/utils.js
+++ default/utils.js
@@ -203,7 +203,7 @@ var utils = {
     },
 
     isLowCPU: function() {
-        return Game.cpu.tickLimit - Game.cpu.getUsed() < (Game.cpu.bucket < Game.cpu.time ? CPU_LIMIT_HIGH : CPU_LIMIT_LOW);
+        return Game.cpu.tickLimit - Game.cpu.getUsed() < (Game.cpu.bucket < Game.cpu.limit ? CPU_LIMIT_HIGH : CPU_LIMIT_LOW);
     },
 };
 
