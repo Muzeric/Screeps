f723417dcc05c74a2f123086ab25c6168abf1490
Muzeric <muzer@mail.ru>
2017-10-22 05:11:03 +0300

    Add my to structures

--- default/prototype.room.js
+++ default/prototype.room.js
@@ -649,6 +649,7 @@ Room.prototype.updateStructures = function() {
             elem.id = s.id;
             elem.pos = s.pos;
             elem.structureType = s.structureType;
+            elem.my = s.my;
             memory.structures[s.structureType] = memory.structures[s.structureType] || [];
             memory.structures[s.structureType].push(elem);
             if (elem.hits < elem.hitsMax * 0.9 && elem.hits < room.getRepairLimit()) {
